1afc486cd0729e2579b79ae694efca4a14ddb9e3
Abhijeet <shindeabhijeet552@gmail.com>
2024-09-06 10:44:07 +0530

    Frontend with voice recognition

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import ChatBox from "./components/ChatBox";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import PdfChat from "./pages/pdfChat";
+// import PdfChat from "./pages/pdfChat";
 
 function App() {
   return (
